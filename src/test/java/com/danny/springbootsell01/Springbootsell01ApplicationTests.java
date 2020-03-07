65c7e87905230d8e9765c4c8edd13611174800fd
cuihaoqi <haoqi8023com>
2020-03-08 01:47:49 +0800

    init

---
+++ src/test/java/com/danny/springbootsell01/Springbootsell01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.springbootsell01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootsell01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
